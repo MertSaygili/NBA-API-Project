c40f8e35e8615bf07c6ebf538b07f8ab54f047cf
MertSaygili <mert71719601@gmail.com>
2022-08-19 00:53:44 +0300

    architectural of project

---
+++ nba_api/lib/theme/custom_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomTheme {
+  late final ThemeData theme;
+
+  CustomTheme() {
+    theme = ThemeData();
+  }
+}
---
+++ nba_api/lib/view/starting_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StartingPageView extends StatefulWidget {
+  const StartingPageView({Key? key}) : super(key: key);
+
+  @override
+  State<StartingPageView> createState() => _StartingPageViewState();
+}
+
+class _StartingPageViewState extends State<StartingPageView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
